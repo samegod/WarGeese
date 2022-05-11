b32550c84c24b78ef08ca4d75fd83c007b368d5d
MaksWar <maksimkiiko2001@gmail.com>
2022-05-11 21:45:51 +0300

    - Infrustructure
    > Зародження інфраструктури проекту

---
+++ Assets/Game/Scripts/Infrastructure/States/LoadLevelState.cs
@@ -0,0 +1,57 @@
+﻿using GameFiles.Scripts.Infrastructure.Factory;
+using Scripts.Infrastructure;
+using Scripts.Logic;
+using UnityEngine;
+
+namespace GameFiles.Scripts.Infrastructure.States
+{
+	public class LoadLevelState : IPayLoadedState<string>
+	{
+		private const string InitialPointTag = "InitialPoint";
+
+		private readonly GameStateMachine _stateMachine;
+		private readonly SceneLoader _sceneLoader;
+		private readonly LoadingCurtain _loadingCurtain;
+		private readonly IGameFactory _gameFactory;
+
+		public LoadLevelState(GameStateMachine stateMachine, SceneLoader sceneLoader, LoadingCurtain loadingCurtain,
+			IGameFactory gameFactory)
+		{
+			_stateMachine = stateMachine;
+			_sceneLoader = sceneLoader;
+			_loadingCurtain = loadingCurtain;
+			_gameFactory = gameFactory;
+		}
+
+		public void Enter(string sceneName)
+		{
+			_loadingCurtain.Show();
+			_sceneLoader.Load(sceneName, OnLoaded);
+		}
+
+		public void Exit() =>
+			_loadingCurtain.Hide();
+
+		private void OnLoaded()
+		{
+			InitGameWorld();
+
+			_stateMachine.Enter<GameLoopState>();
+		}
+
+		private void InitGameWorld()
+		{
+			//var character = _gameFactory.CreateHero(at: GameObject.FindWithTag(InitialPointTag));
+			//_gameFactory.CreateHUD();
+
+			//CameraFollow(character);
+		}
+
+		private void CameraFollow(GameObject character)
+		{
+			/*Camera.main
+				.GetComponent<CameraController>()
+				.Follow(character.transform);*/
+		}
+	}
+}
---
+++ Assets/Game/Scripts/Logic/LoadingCurtain.cs
@@ -0,0 +1,30 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Scripts.Logic
+{
+	public class LoadingCurtain : MonoBehaviour
+	{
+		[SerializeField] private CanvasGroup curtain;
+
+		private const float DurationOfFade = 3f;
+
+		private void Awake()
+		{
+			DontDestroyOnLoad(this);
+		}
+
+		public void Show()
+		{
+			gameObject.SetActive(true);
+			curtain.alpha = 1;
+		}
+
+		public void Hide()
+		{
+			curtain
+				.DOFade(0, DurationOfFade)
+				.onComplete = () => gameObject.SetActive(false);
+		}
+	}
+}

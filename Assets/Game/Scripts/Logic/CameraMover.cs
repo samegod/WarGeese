af71bd6ef63e668b5ca5b1956ee11365b770648b
MaksWar <maksimkiiko2001@gmail.com>
2022-05-21 20:12:50 +0300

    - MapGenerator
    > Додані елементи на мапу
    > Додан скрипт для автоматичного руху камери

---
+++ Assets/Game/Scripts/Logic/CameraMover.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Scripts.Logic
+{
+	public class CameraMover : MonoBehaviour
+	{
+		[SerializeField] private float speedX;
+		[SerializeField] private float speedY;
+		[SerializeField] private float speedZ;
+
+		private Camera _camera;
+
+		private void Awake() =>
+			_camera = Camera.main;
+
+		private void Update() =>
+			_camera.transform.Translate(new Vector3(speedX , speedY, speedZ) * Time.deltaTime);
+	}
+}
